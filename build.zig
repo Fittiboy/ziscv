18cf7a505d44bbbbfff16d3768c30096b6fa49d9
Fitti <dev@fitti.io>
2026-06-09 15:11:33 +0200

    Rename simulator in build.zig

--- build.zig
+++ build.zig
@@ -4,10 +4,10 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "ziscv",
+    const simulator = b.addExecutable(.{
+        .name = "ziscv-simulator",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/simulator.zig"),
             .target = target,
             .optimize = optimize,
         }),
@@ -22,21 +22,21 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    b.installArtifact(exe);
+    b.installArtifact(simulator);
     b.installArtifact(assembler_exe);
 
-    const run_step = b.step("run", "Run the Simulator");
-    const run = b.addRunArtifact(exe);
-    run.addPassthruArgs();
-    run_step.dependOn(&run.step);
+    const simulator_run_step = b.step("simulate", "Run the Simulator");
+    const simulator_run = b.addRunArtifact(simulator);
+    simulator_run.addPassthruArgs();
+    simulator_run_step.dependOn(&simulator_run.step);
 
     const assembler_run_step = b.step("assemble", "Run the Assembler");
     const assembler_run = b.addRunArtifact(assembler_exe);
     assembler_run.addPassthruArgs();
     assembler_run_step.dependOn(&assembler_run.step);
 
-    const exe_test = b.addTest(.{
-        .root_module = exe.root_module,
+    const simulator_test = b.addTest(.{
+        .root_module = simulator.root_module,
         .name = "assembler_test",
         .use_llvm = true,
     });
@@ -47,15 +47,15 @@ pub fn build(b: *std.Build) void {
         .use_llvm = true,
     });
 
-    const exe_test_step = b.step("test-simulator", "Test the simulator");
-    const exe_test_run = b.addRunArtifact(exe_test);
-    exe_test_step.dependOn(&exe_test_run.step);
+    const simulator_test_step = b.step("test-simulator", "Test the simulator");
+    const simulator_test_run = b.addRunArtifact(simulator_test);
+    simulator_test_step.dependOn(&simulator_test_run.step);
 
     const assembler_test_step = b.step("test-assembler", "Test the assembler");
     const assembler_test_run = b.addRunArtifact(assembler_test);
     assembler_test_step.dependOn(&assembler_test_run.step);
 
     const test_step = b.step("test", "Test both the assembler and the simulator");
-    test_step.dependOn(&exe_test_run.step);
+    test_step.dependOn(&simulator_test_run.step);
     test_step.dependOn(&assembler_test_run.step);
 }
--- src/assembler.zig
+++ src/assembler.zig
@@ -13,6 +13,8 @@ pub fn main(init: std.process.Init) !void {
     const file = cwd.openFile(io, filename, .{ .allow_directory = false }) catch |err| {
         std.process.fatal("when trying to open file \"{s}\": {s}", .{ filename, @errorName(err) });
     };
+    defer file.close(io);
+
     var reader_buf: [1024]u8 = undefined;
     var file_reader = file.reader(io, &reader_buf);
     const reader: *std.Io.Reader = &file_reader.interface;
