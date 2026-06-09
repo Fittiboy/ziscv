636122b0fb7b7255085bf307b8ba68ec038c8d47
Fitti <dev@fitti.io>
2026-06-09 15:28:55 +0200

    Introduce simulated Machine

--- src/simulator.zig
+++ src/simulator.zig
@@ -23,6 +23,35 @@ pub fn main(init: std.process.Init) !void {
     var stdout_writer: std.Io.File.Writer = .init(.stdout(), io, &stdout_buf);
     const stdout: *std.Io.Writer = &stdout_writer.interface;
 
-    _ = try reader.streamRemaining(stdout);
+    var machine: Machine = try .init(reader);
+
+    try machine.simulate();
+    const memory: *[32 * 4 * 1024 * 4]u8 = @ptrCast(&machine.memory);
+
+    try stdout.writeAll(memory);
     try stdout.flush();
 }
+
+pub const Machine = struct {
+    const Self = @This();
+
+    prog_len: usize = 0,
+    pc: u32 = 0x00000000,
+    register_bank: [32]u32 = @splat(0x00000000),
+    memory: [32 * 4 * 1024]u32 = undefined,
+
+    const fresh: Self = .{};
+
+    pub fn init(prog_reader: *std.Io.Reader) !Self {
+        var machine: Self = .fresh;
+        const memory: *[32 * 4 * 1024 * 4]u8 = @ptrCast(&machine.memory);
+        var mem_writer: std.Io.Writer = .fixed(memory);
+        machine.prog_len = try prog_reader.streamRemaining(&mem_writer);
+
+        return machine;
+    }
+
+    pub fn simulate(self: *Self) !void {
+        _ = self;
+    }
+};
