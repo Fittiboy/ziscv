92bad771aa407e20bbb01bf0c7627774a10ead74
Fitti <dev@fitti.io>
2026-06-13 17:09:47 +0200

    Upgrade hello.s example

--- src/hello.s
+++ src/hello.s
@@ -1,35 +1,39 @@
-addi x1, x0, 144
-addi x2, x0, 72
-addi x3, x0, 101
-addi x4, x0, 108
-addi x5, x0, 108
-addi x6, x0, 111
-addi x7, x0, 44
-addi x8, x0, 32
-addi x9, x0, 83
-addi x10, x0, 105
-addi x11, x0, 109
-addi x12, x0, 117
-addi x13, x0, 108
-addi x14, x0, 97
-addi x15, x0, 116
-addi x16, x0, 111
-addi x17, x0, 114
-addi x18, x0, 33
-sw x2, 0(x1)
-sw x3, 4(x1)
-sw x4, 8(x1)
-sw x5, 12(x1)
-sw x6, 16(x1)
-sw x7, 20(x1)
-sw x8, 24(x1)
-sw x9, 28(x1)
-sw x10, 32(x1)
-sw x11, 36(x1)
-sw x12, 40(x1)
-sw x13, 44(x1)
-sw x14, 48(x1)
-sw x15, 52(x1)
-sw x16, 56(x1)
-sw x17, 60(x1)
-sw x18, 64(x1)
+prep:
+    addi x1, zero, data
+    addi x2, zero, 72
+    addi x3, zero, 101
+    addi x4, zero, 108
+    addi x5, zero, 108
+    addi x6, zero, 111
+    addi x7, zero, 44
+    addi x8, zero, 32
+    addi x9, zero, 83
+    addi x10, zero, 105
+    addi x11, zero, 109
+    addi x12, zero, 117
+    addi x13, zero, 108
+    addi x14, zero, 97
+    addi x15, zero, 116
+    addi x16, zero, 111
+    addi x17, zero, 114
+    addi x18, zero, 33
+
+memory_writes:
+    sw x2, 0(x1)
+    sw x3, 4(x1)
+    sw x4, 8(x1)
+    sw x5, 12(x1)
+    sw x6, 16(x1)
+    sw x7, 20(x1)
+    sw x8, 24(x1)
+    sw x9, 28(x1)
+    sw x10, 32(x1)
+    sw x11, 36(x1)
+    sw x12, 40(x1)
+    sw x13, 44(x1)
+    sw x14, 48(x1)
+    sw x15, 52(x1)
+    sw x16, 56(x1)
+    sw x17, 60(x1)
+    sw x18, 64(x1)
+data:
